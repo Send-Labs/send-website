f07433f9b658cdf88fe191f61b36d6bdb5d44d64
doctor <doctor192322@gmail.com>
2024-03-02 21:26:44 +0800

    update twitter url

--- src/layouts/Footer/index.tsx
+++ src/layouts/Footer/index.tsx
@@ -1,17 +1,43 @@
-import styles from './index.less'
+import styles from "./index.less";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-        <div className={styles.footerTop}>
-            <span>Powered by Way Network</span>
-        </div>
-        <div className={styles.footerBot}>
-            <a target="_blank" href="https://discord.gg/7vDHh5EeEK">Discord</a>
-            <a target="_blank" href="https://send-finance.gitbook.io/untitled/"  style={{marginLeft: '24px'}}>Gitbook</a>
-            <a target="_blank" href="https://github.com/Send-Labs" style={{marginLeft: '24px'}}>Github</a>
-            <a target="_blank" href="https://twitter.com/Send_Finance_" style={{marginLeft: '24px'}}>Twitter</a>
-            <a target="_blank" href="https://t.me/Send_Finance" style={{marginLeft: '24px'}}>Telegram</a>
-        </div>
+      <div className={styles.footerTop}>
+        <span>Powered by Way Network</span>
+      </div>
+      <div className={styles.footerBot}>
+        <a target="_blank" href="https://discord.gg/7vDHh5EeEK">
+          Discord
+        </a>
+        <a
+          target="_blank"
+          href="https://send-finance.gitbook.io/untitled/"
+          style={{ marginLeft: "24px" }}
+        >
+          Gitbook
+        </a>
+        <a
+          target="_blank"
+          href="https://github.com/Send-Labs"
+          style={{ marginLeft: "24px" }}
+        >
+          Github
+        </a>
+        <a
+          target="_blank"
+          href="https://twitter.com/Send_Labs"
+          style={{ marginLeft: "24px" }}
+        >
+          Twitter
+        </a>
+        <a
+          target="_blank"
+          href="https://t.me/Send_Finance"
+          style={{ marginLeft: "24px" }}
+        >
+          Telegram
+        </a>
+      </div>
     </footer>
-  )
+  );
 }
